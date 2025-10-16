fd9028391e07d4f9fe8636e08da6ba4abc3f7e5a
Jihyun <jose1123@naver.com>
2025-10-16 16:43:06 +0900

    feat: 기본 구분자로 문자열 분리 후 합산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,15 +11,21 @@ public class Application {
         String input = br.readLine();
         //String input = null;
         //String input = "";
+        //String input = "1:2,3";
 
-        int answer = 0;
-        //빈 문자열이나 null을 입력받으면 답은 0
+        int sum = 0;
+        //빈 문자열이나 null을 입력받으면 0
         if(input == null || input.trim().equals("")) { //isEmpty()도 가능. trim 해주는 것도 일종의 예외 처리?.null부터 체크해줘야함!!
-            answer =0;
-
+            sum =0;
+        }
+        //기본 구분자(:,)로 문자열 분리
+        String[] numbers = input.split("[:,]");//정규식 표현
+        //합산
+        for(String number : numbers) {
+            sum+=Integer.parseInt(number); //문자열을 숫자로 바꾼 후 더함
         }
         //출력
-        System.out.println(answer);
+        System.out.println(sum);
 
     }
 }
