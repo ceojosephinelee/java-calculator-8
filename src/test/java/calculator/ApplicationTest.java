ec1ea99a3a2eaa4a288c82f7865185b731cb965e
Jihyun <jose1123@naver.com>
2025-10-20 14:47:39 +0900

    test : 정상테스트와 예외테스트코드 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -15,6 +15,27 @@ class ApplicationTest extends NsTest {
             assertThat(output()).contains("결과 : 1");
         });
     }
+    @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1:3,2");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+    @Test
+    void 커스텀_구분자_사용2() {
+        assertSimpleTest(() -> {
+            run("// \\n1 2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+    @Test
+    void 커스텀_구분자_사용3() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;3,1");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
 
     @Test
     void 예외_테스트() {
@@ -23,6 +44,39 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+    @Test
+    void 예외_테스트_추가1(){
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("1;2,3"))
+                .isInstanceOf(IllegalArgumentException.class)
+
+        );
+    }
+    @Test
+    void 예외_테스트_추가2(){
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//\n1,2,3"))
+                    .isInstanceOf(IllegalArgumentException.class)
+
+        );
+    }
+    @Test
+    void 예외_테스트_추가3(){
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//; \n1;2,3"))
+                    .isInstanceOf(IllegalArgumentException.class)
+
+        );
+    }
+    @Test
+    void 예외_테스트_추가4(){
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//3\n1,233"))
+                    .isInstanceOf(IllegalArgumentException.class)
+
+        );
+    }
+
 
     @Override
     public void runMain() {
